ed750e06b6c3d942ffc10977fdfb29d447c7390b
Alex Blanford <alexblanford17@gmail.com>
2025-03-28 17:31:46 +0000

    added loadCars function

--- CarShowroom.py
+++ CarShowroom.py
@@ -1,21 +1,42 @@
 import tkinter as tk
 from tkinter import messagebox
 
+carFile = "car_listings.txt"
+
 root = tk.Tk()
+root.title("Car Showroom")
+root.geometry("900x700")
 
 
 
+titleName = tk.Label(root, text="Car Showroom", font=("Helvetica", 30, "bold"))
+titleName.pack(pady=20)
 
-titleName = tk.Label(root, text="Car Showroom", font=("Helvetica", 25))
-titleName.pack(padx = 10, pady = 10)
+bottomFrame = tk.Frame(root)
+bottomFrame.pack(side="bottom")
+bottomFrame.pack(padx = 400, pady = 200)
 
 
-
-
-tk.Button(root, text="View Cars", fg="blue", width=15, height = 2).pack(side="left", padx=10)
-tk.Button(root, text="Add Car", fg="red", width = 15, height = 2).pack(side="left", padx=10)
+tk.Button(root, text="View Cars", fg="blue", width=15, height = 3).pack(side="left", padx=10)
+tk.Button(root, text="Add Car", fg="red", width = 15, height = 3).pack(side="left", padx=10)
 frame = tk.Frame(root)
 frame.pack(padx=10, pady=100, expand = True)
 
 
+def loadCars():
+    with open(carFile, "r") as file:
+        cars = []
+        for line in file.readlines():
+            parts = line.strip().split(",")
+            if len(parts) == 5:
+                cars.append({
+                    "make": parts[0],
+                    "model": parts[1],
+                    "year": parts[2],
+                    "price": parts[3],
+                    "image": parts[4]
+                })
+
+loadCars()
 root.mainloop()
+
